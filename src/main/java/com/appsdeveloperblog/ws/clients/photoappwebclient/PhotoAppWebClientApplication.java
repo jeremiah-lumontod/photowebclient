5c7bc711185ee6014246c4fa3bb7aaa26fe3d7be
Jerry Lumontod <>
2024-01-29 16:12:34 +0800

    display jwt token

--- src/main/java/com/appsdeveloperblog/ws/clients/photoappwebclient/PhotoAppWebClientApplication.java
+++ src/main/java/com/appsdeveloperblog/ws/clients/photoappwebclient/PhotoAppWebClientApplication.java
@@ -2,12 +2,16 @@ package com.appsdeveloperblog.ws.clients.photoappwebclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/*import org.springframework.context.annotation.Bean;
+
+/*
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;*/
+
+*/
 
 @SpringBootApplication
 public class PhotoAppWebClientApplication {
@@ -16,12 +20,13 @@ public class PhotoAppWebClientApplication {
 		SpringApplication.run(PhotoAppWebClientApplication.class, args);
 	}
 	
-/*
-	@Bean
+
+/*	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
-	}
-	
+	}*/
+
+/*
 	@Bean
 	public WebClient webClient(ClientRegistrationRepository clientRegistrationrepository,
 			OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository
--- src/main/java/com/appsdeveloperblog/ws/clients/photoappwebclient/controllers/AlbumsController.java
+++ src/main/java/com/appsdeveloperblog/ws/clients/photoappwebclient/controllers/AlbumsController.java
@@ -1,36 +1,48 @@
 package com.appsdeveloperblog.ws.clients.photoappwebclient.controllers;
 
 import java.util.Arrays;
-import java.util.List;
 
 import com.appsdeveloperblog.ws.clients.photoappwebclient.response.AlbumRest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 
 
 @Controller
 public class AlbumsController {
-	
-/*	@Autowired
-	OAuth2AuthorizedClientService oauth2ClientService;*/
-	
-//	@Autowired
-//	RestTemplate restTemplate;
 
-/*
+	private final OAuth2AuthorizedClientService oauth2ClientService;
+
 	@Autowired
-	WebClient webClient;
-*/
+	public AlbumsController(OAuth2AuthorizedClientService oauth2ClientService) {
+		this.oauth2ClientService = oauth2ClientService;
+	}
 
-/*
 	@GetMapping("/albums")
 	public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
-*/
 
-	@GetMapping("/albums")
-	public String getAlbums(Model model) {
+		System.out.println("Principal: " + principal);
+		OidcIdToken idToken = principal.getIdToken();
+		String idTokenValue = idToken.getTokenValue();
+		System.out.println("idTokenValue: " + idTokenValue);
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+
+		OAuth2AuthorizedClient oauth2Client = oauth2ClientService.loadAuthorizedClient(oauthToken.getAuthorizedClientRegistrationId(), oauthToken.getName());
+
+		String jwtAccessToken = oauth2Client.getAccessToken().getTokenValue();
+		System.out.println("\n\n\n");System.out.println("\n\n\n");System.out.println("\n\n\n");
+		System.out.println("jwtAccessToken = " + jwtAccessToken);
 
 		AlbumRest album = new AlbumRest();
 		album.setAlbumId("albumOne");
